ef9e8b77c6f90d6096288b66d3cf47fdc86d896c
Kevin Oberlies <favilo@gmail.com>
2010-12-27 02:32:48 -0800

    small changes here.

--- src/Chonquest.hs
+++ src/Chonquest.hs
@@ -1,10 +1,21 @@
 module Chonquest where
 
 import Data.Array.IArray
+
+import Control.Monad
 import Control.Monad.State
 import Control.Monad.Reader
 
 type CellArray = Array Int Cell
+type Coord     = (Int, Int)
+type AppState  = StateT Board IO
+type AppEnv    = ReaderT AppConfig AppState
+
+data AppConfig = AppConfig
+    { 
+      board    :: Board
+    }
+    deriving (Show, Eq)
 
 data Owner = Vacant
     | Neutral
@@ -15,20 +26,32 @@ data PlayerType = Human
     | EasyComputer
     | MediumComputer
     | HardComputer
-    deriving (Show, Eq)
+    deriving (Show, Eq, Enum, Ord)
 
 data Player = Player 
-    { name   :: String
+    { p_name :: String
     , number :: Int
-    , t_player :: PlayerType
+    , p_type :: PlayerType
     }
+    deriving (Show, Eq)
+
+data Fleet = Fleet 
+    { n_ships :: Int
+    , origin  :: Cell
+    , target  :: Cell
+    }
+    deriving (Show, Eq)
 
 data Cell = Cell 
     { icon   :: Int
     , owner  :: Owner
+    , c_name   :: String
     , kill_p :: Double
     , prod   :: Int 
+    , ships  :: Int
+    , pos    :: Coord
     }
+    deriving (Show, Eq)
 
 data Board = Board 
     { cells       :: CellArray
@@ -39,6 +62,11 @@ data Board = Board
     , cum_prod    :: Bool
     , p_after_cap :: Bool
     , players     :: [Player]
+    , fleets      :: [Fleet]
     } 
+    deriving (Show, Eq)
 
+initEnv :: IO AppConfig
+initEnv = do
+    return undefined
 
